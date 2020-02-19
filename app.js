2e5fab29ebcf32ffcbcdc0ece0b1972c931ca75f
Nicholas Roberto <njr444@gmail.com>
2020-02-19 11:17:52 -0800

    weather update

--- app.js
+++ app.js
@@ -4,26 +4,38 @@ const app = express();
 const weather = require('./Data/darkSky.json');
 const port = process.env.PORT || 3000;
 const geoData = require('./Data/geo.json');
-// const cors = require('cors');
+const cors = require('cors');
+
+app.use(cors);
+
+let lat;
+let lng;
+
 
 getWeatherData(lat, lng){
     weather.daily.data.map(forecast =>{
         return { 
             forecast: forecast.summary,
-            time: new Date(forecast.time),  
+            time: new Date(forecast.time * 1000),  
         };   
     })
 };
 
 app.get('/weather/', (req, res) => {
-    const portlandWeather = getWeatherData();
+    const portlandWeather = getWeatherData(lat, lng);
 
     res.json(portlandWeather);
 });
 
 
 app.get('/location/', (req, res) => {
+    const location = request.query.search;
+    
+    lat = cityData.geometry.location.lat;
+    lng = cityData.geometry.location.lng;
+
     const cityData = geoData.results[0];
+
     res.json(
         {
             formatted_query: cityData.formatted_address,
