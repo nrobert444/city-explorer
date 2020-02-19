bdfea2449c55579af507ae510c6e555fd75086dc
Nicholas Roberto <njr444@gmail.com>
2020-02-19 10:13:37 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const request = require('superagent');
+const app = express();
+const darkSky = require('./data/darksky.json');
+const port = process.env.PORT || 3000;
+const geoData = require('./data/geo.json');
+
+
+app.get('/weather/', (request, respond) => {
+    const forecast = darkSky.daily.data;
+
+    respond.json({
+        forecast: forecast.summary,
+        time: forecast.time
+    });
+});
+
+app.get('/location/', (request, respond) => {
+    const cityData = geoData.results[0];
+    respond.json(
+        {
+            formatted_query: cityData.formatted_address,
+            latitude: cityData.geometry.location.lat,
+            longitude: cityData.geometry.location.lng
+        });
+});
+// has to go into it's own index.js file for testing later
+app.listen(port, () => {
+    console.log('<-----------blast off!---------------->');
+});
+
+
+app.get('*', (req, res) => res.send('404 error buddy!!!!!!'));
+
+
+
+
+// module.exports = {
+//     app, };
